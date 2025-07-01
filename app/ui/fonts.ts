9ab4522aebcf8af5d85db49b243405954c24b3b0
Dong Liu <dliu.cn@gmail.com>
2025-07-01 01:27:41 -0400

    add subset

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter } from 'next/font/google';
 import { Lusitana  } from 'next/font/google'; 
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight:"700" });
+export const lusitana = Lusitana({ weight:"700", subsets: ['latin'] });
